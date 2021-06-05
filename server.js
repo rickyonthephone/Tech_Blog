1ad095159e537bd0a2d062aa71a129e6e629db40
Richard Crippen <richard.crippen@gmail.com>
2021-06-05 12:41:27 -0400

    continuing basic files with db schema, connect.js, etc... verified that server is listening when server.js is run.

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const exphbs = require('express-handlebars');
 // Expect to use controllers and helper functions - but currently none to import
 // const routes = require('./controllers');
 // const helpers = require('./utils/helpers');
-const sequelize = require('./config/connection');
+const sequelize = require('./config/connect');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
