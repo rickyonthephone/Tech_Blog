9a13fd91b0946e75bad45b32c04e7861823bf9bf
Richard Crippen <richard.crippen@gmail.com>
2021-06-05 12:28:36 -0400

    small changes to initial files and create package.json file, overlooked before

--- server.js
+++ server.js
@@ -2,7 +2,19 @@ const path = require('path');
 const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
-const routes = require('./controllers');
-const helpers = require('./utils/helpers');
+// Expect to use controllers and helper functions - but currently none to import
+// const routes = require('./controllers');
+// const helpers = require('./utils/helpers');
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const app = express();
+const PORT = process.env.PORT || 8080
+
+
+
+
+
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log('Now listening'));
+  });
