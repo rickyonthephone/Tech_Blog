fcc93cec62e548593596e98a526fa13efa1cb021
Richard Crippen <richard.crippen@gmail.com>
2021-06-05 19:06:08 -0400

    setting up views, layouts, and adjusting server.js data to use handlebars - will work on this tomorrow.

--- server.js
+++ server.js
@@ -3,14 +3,22 @@ const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
 // Expect to use controllers and helper functions - but currently none to import
-// const routes = require('./controllers');
-// const helpers = require('./utils/helpers');
+const routes = require('./routeControls');
+const helpers = require('./utils/helpers');
 const sequelize = require('./config/connect');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
 const PORT = process.env.PORT || 8080
 
+const hbs = exphbs.create({ helpers });
+
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
 
 
 
