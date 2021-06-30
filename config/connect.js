c24c8c799105efda99e5ad5d60ba6fa958898200
Richard Crippen <richard.crippen@gmail.com>
2021-06-30 15:00:30 -0400

    small changes

--- config/connect.js
+++ config/connect.js
@@ -2,6 +2,7 @@ const Sequelize = require('sequelize');
 require('dotenv').config();
 
 const sequelize = process.env.JAWSDB_URL
+
 ? new Sequelize(process.env.JAWSDB_URL)
 : new Sequelize(process.env.MYSQL_URL);
 
