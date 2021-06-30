66def1dab42885ec7c309e9b243710a40faa430c
Richard Crippen <richard.crippen@gmail.com>
2021-06-30 17:26:56 -0400

    tried different code for the connection file

--- config/connect.js
+++ config/connect.js
@@ -1,15 +1,4 @@
-// const Sequelize = require('sequelize');
-// require('dotenv').config();
 
-// let sequelize = process.env.JAWSDB_ROSE_URL
-
-// ? new Sequelize(process.env.JAWSDB_ROSE_URL)
-// : new Sequelize(process.env.MYSQL_URL);
-
-// module.exports = sequelize;
-
-// Dependencies
-// Sequelize constructor
 const Sequelize = require('sequelize');
 // dotenv for local environmental variables for user name and password
 require('dotenv').config();
