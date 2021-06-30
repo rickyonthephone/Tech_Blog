715166bdfcd0515daaea0636f91ac29f3207bd7a
Richard Crippen <richard.crippen@gmail.com>
2021-06-30 17:24:14 -0400

    no, connection file was not the problem

--- config/connect.js
+++ config/connect.js
@@ -1,9 +1,29 @@
-const sequelize = require('sequelize');
+// const Sequelize = require('sequelize');
+// require('dotenv').config();
+
+// let sequelize = process.env.JAWSDB_ROSE_URL
+
+// ? new Sequelize(process.env.JAWSDB_ROSE_URL)
+// : new Sequelize(process.env.MYSQL_URL);
+
+// module.exports = sequelize;
+
+// Dependencies
+// Sequelize constructor
+const Sequelize = require('sequelize');
+// dotenv for local environmental variables for user name and password
 require('dotenv').config();
 
-const sequelize = process.env.JAWSDB_ROSE_URL
+let sequelize;
 
-? new Sequelize(process.env.JAWSDB_ROSE_URL)
-: new Sequelize(process.env.MYSQL_URL);
+if (process.env.JAWSDB_ROSE_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_ROSE_URL);
+} else {
+  sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+    host: 'localhost',
+    dialect: 'mysql',
+    port: 3306
+  });
+}
 
 module.exports = sequelize;
